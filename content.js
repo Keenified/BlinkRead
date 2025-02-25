7318614e5cfb1245928da4472cd7cf35f633e2e3
WilliamBoulton <will@willboulton.com>
2025-02-25 19:15:02 +0000

    delete em

--- content.js
+++
@@ -1,3 +0,0 @@
-// This file is needed for the extension to work with the context menu
-// It's intentionally empty as we don't need content script functionality
-// but the manifest requires it for the context menu to work
